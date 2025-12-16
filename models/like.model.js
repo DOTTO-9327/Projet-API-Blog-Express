859efe6f7e825b1d81bf467649a12b55a158ce88
alyball78 <aly.ball@laplateforme.io>
2025-12-16 14:25:18 +0100

    medel like ok

--- models/like.model.js
+++ models/like.model.js
@@ -1,8 +1,6 @@
 const db = require("../config/database");
-const create = (user_id, article_id)=>{
-const sql = "INSERT INTO article_user_like(user_id, article_id) VALUES(?,?)";
-db.query(sql,[user_id, article_id ],callback);
-
-
-}
-module.exports ={create};
+const create = (user_id, article_id, callback) => {
+const sql = "INSERT INTO article_user_like (user_id, article_id) VALUES (?,?)";
+db.query(sql, [user_id, article_id], callback);
+};
+module.exports = {create};
